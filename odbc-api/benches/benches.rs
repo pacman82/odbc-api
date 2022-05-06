1874a5e6ff909a3581929d1092677f072462abf4
Markus Klein <markus-klein@live.de>
2022-05-06 23:01:16 +0200

    add benchmark for buffer allocation

---
+++ odbc-api/benches/benches.rs
@@ -0,0 +1,25 @@
+use std::iter;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use odbc_api::buffers::{buffer_from_description, BufferDescription};
+
+fn falliable_buffer_allocation(capacity: usize, max_str_len: usize) {
+    let description = BufferDescription {
+        kind: odbc_api::buffers::BufferKind::Text { max_str_len },
+        nullable: true,
+    };
+    buffer_from_description(capacity, iter::once(description)).unwrap();
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("fallibale buffer allocation", |b| {
+        b.iter(|| {
+            let capacity = 1000;
+            let max_str_len = 65536;
+            falliable_buffer_allocation(black_box(capacity), black_box(max_str_len))
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
