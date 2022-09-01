10c36214b62050e23b370d72d3b0a2e45207641e
Markus Klein <markus-klein@live.de>
2022-09-01 08:02:14 +0200

    better code example

--- odbc-api/src/preallocated.rs
+++ odbc-api/src/preallocated.rs
@@ -212,7 +212,8 @@ impl<'o> AsStatementRef for Preallocated<'o> {
     }
 }
 
-/// Asynchronous sibling of [`Preallocated`] using polling mode for execution.
+/// Asynchronous sibling of [`Preallocated`] using polling mode for execution. Can be obtained using
+/// [`Preallocated::into_polling`].
 pub struct PreallocatedPolling<'open_connection> {
     /// A valid statement handle in polling mode
     statement: StatementImpl<'open_connection>,
@@ -223,6 +224,47 @@ impl<'o> PreallocatedPolling<'o> {
         Self { statement }
     }
 
+    /// Executes a statement. This is the fastest way to sequentially execute different SQL
+    /// Statements asynchronously.
+    ///
+    /// # Parameters
+    ///
+    /// * `query`: The text representation of the SQL statement. E.g. "SELECT * FROM my_table;".
+    /// * `params`: `?` may be used as a placeholder in the statement text. You can use `()` to
+    ///   represent no parameters. Check the [`crate::parameter`] module level documentation for
+    ///   more information on how to pass parameters.
+    /// * `sleep`: Governs the polling intervals
+    ///
+    /// # Return
+    ///
+    /// Returns `Some` if a cursor is created. If `None` is returned no cursor has been created (
+    /// e.g. the query came back empty). Note that an empty query may also create a cursor with zero
+    /// rows. Since we want to reuse the statement handle a returned cursor will not take ownership
+    /// of it and instead burrow it.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use odbc_api::{Connection, Error};
+    /// use std::io::{self, stdin, Read};
+    ///
+    /// /// Execute many different queries sequentially.
+    /// async fn execute_all(conn: &Connection, queries: &[&str]) -> Result<(), Error>{
+    ///     let statement = conn.preallocate()?.into_polling()?;
+    ///     let sleep = || tokio::time::sleep(Duration::from_millis(20));
+    ///     for query in queries {
+    ///         println!("Executing {query}");
+    ///         match statement.execute(&query, (), sleep) {
+    ///             Err(e) => println!("{}", e),
+    ///             Ok(None) => println!("No results set generated."),
+    ///             Ok(Some(cursor)) => {
+    ///                 // ...print cursor contents...
+    ///             },
+    ///         }
+    ///     }
+    ///     Ok(())
+    /// }
+    /// ```
     pub async fn execute(
         &mut self,
         query: &str,
