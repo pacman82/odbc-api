c1add633bf1b2e2cfc3c62e226cd57948c8ca145
Markus Klein <markus-klein@live.de>
2023-09-10 16:28:25 +0200

    add narrow wrapper

---
+++ odbc-api/src/narrow.rs
@@ -0,0 +1,65 @@
+use crate::{IntoParameter, parameter::{VarCharSlice, VarCharBox}};
+
+/// Newtype wrapper intend to be used around `String`s or `str` slices to bind them always as narrow
+/// text independent of wether the `narrow` feature is set or not.
+pub struct Narrow<T>(pub T);
+
+impl<'a> IntoParameter for Narrow<&'a str> {
+    type Parameter = VarCharSlice<'a>;
+
+    fn into_parameter(self) -> Self::Parameter {
+        VarCharSlice::new(self.0.as_bytes())
+    }
+}
+
+impl<'a> IntoParameter for Narrow<Option<&'a str>> {
+    type Parameter = VarCharSlice<'a>;
+
+    fn into_parameter(self) -> Self::Parameter {
+        match self.0 {
+            Some(str) => str.into_parameter(),
+            None => VarCharSlice::NULL,
+        }
+    }
+}
+
+impl<'a> IntoParameter for Option<Narrow<&'a str>> {
+    type Parameter = VarCharSlice<'a>;
+
+    fn into_parameter(self) -> Self::Parameter {
+        match self {
+            Some(str) => str.0.into_parameter(),
+            None => VarCharSlice::NULL,
+        }
+    }
+}
+
+impl IntoParameter for Narrow<String> {
+    type Parameter = VarCharBox;
+
+    fn into_parameter(self) -> Self::Parameter {
+        VarCharBox::from_string(self.0)
+    }
+}
+
+impl IntoParameter for Narrow<Option<String>> {
+    type Parameter = VarCharBox;
+
+    fn into_parameter(self) -> Self::Parameter {
+        match self.0 {
+            Some(str) => str.into_parameter(),
+            None => VarCharBox::null(),
+        }
+    }
+}
+
+impl IntoParameter for Option<Narrow<String>> {
+    type Parameter = VarCharBox;
+
+    fn into_parameter(self) -> Self::Parameter {
+        match self {
+            Some(str) => str.0.into_parameter(),
+            None => VarCharBox::null(),
+        }
+    }
+}
