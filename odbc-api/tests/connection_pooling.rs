21b2d4e2960cae73d384661d618948c77be5d170
Markus Klein <markus-klein@live.de>
2024-06-23 10:52:06 +0200

    tests: generalize connection pooling test

--- odbc-api/tests/connection_pooling.rs
+++ odbc-api/tests/connection_pooling.rs
@@ -4,10 +4,38 @@
 use lazy_static::lazy_static;
 use odbc_api::{ConnectionOptions, Environment};
 use odbc_sys::{AttrConnectionPooling, AttrCpMatch};
+use test_case::test_case;
 
 const MSSQL_CONNECTION: &str =
     "Driver={ODBC Driver 18 for SQL Server};Server=localhost;UID=SA;PWD=My@Test@Password1;\
     TrustServerCertificate=yes;";
+
+#[cfg(target_os = "windows")]
+const SQLITE_3_CONNECTION: &str =
+    "Driver={SQLite3 ODBC Driver};Database=sqlite-test.db;{Journal Mode}=WAL;";
+#[cfg(not(target_os = "windows"))]
+const SQLITE_3_CONNECTION: &str = "Driver={SQLite3};Database=sqlite-test.db;{Journal Mode}=WAL;";
+
+#[cfg(target_os = "windows")]
+const MARIADB_CONNECTION: &str = "Driver={MariaDB ODBC 3.1 Driver};\
+    Server=localhost;DB=test_db;\
+    UID=root;PWD=my-secret-pw;\
+    Port=3306";
+
+// Use 127.0.0.1 instead of localhost so the system uses the TCP/IP connector instead of the socket
+// connector. Prevents error message: 'Can't connect to local MySQL server through socket'.
+#[cfg(not(target_os = "windows"))]
+const MARIADB_CONNECTION: &str = "Driver={MariaDB 3.1 Driver};\
+    Server=127.0.0.1;DB=test_db;\
+    UID=root;PWD=my-secret-pw;\
+    Port=3306";
+
+const POSTGRES_CONNECTION: &str = "Driver={PostgreSQL UNICODE};\
+    Server=localhost;\
+    Port=5432;\
+    Database=test;\
+    Uid=test;\
+    Pwd=test;";
 
 // Rust by default executes tests in parallel. Yet only one environment is allowed at a time.
 lazy_static! {
@@ -21,13 +49,16 @@ lazy_static! {
     };
 }
 
-#[test]
-fn connect() {
+#[test_case(MSSQL_CONNECTION; "Microsoft SQL Server")]
+#[test_case(MARIADB_CONNECTION; "Maria DB")]
+#[test_case(SQLITE_3_CONNECTION; "SQLite 3")]
+#[test_case(POSTGRES_CONNECTION; "PostgreSQL")]
+fn connect(connection_string: &str) {
     // First connection should be created on demand
     {
         let conn = ENV
             .connect_with_connection_string(
-                MSSQL_CONNECTION,
+                connection_string,
                 // Fail faster if we forgot to boot up docker containers
                 ConnectionOptions {
                     login_timeout_sec: Some(2),
