2d9a3d7ad06f9614ea13adafce3eb72a1c723b3f
Markus Klein <markus-klein@live.de>
2024-06-23 10:10:19 +0200

    Fix dead code warning

--- derive/tests/derive.rs
+++ derive/tests/derive.rs
@@ -1,6 +1,11 @@
 use odbc_api::parameter::VarCharArray;
 use odbc_api_derive::Fetch;
 
+// A check, wether the derive syntax produces something that compiles. For a test actually fetching
+// date from a database using this generated code, run the integration tests of `odbc-api` with the
+// `derive` feature activated. We allow dead code here, because we do not intend to invoke the
+// implementation here.
+#[allow(dead_code)]
 #[derive(Fetch, Clone, Copy)]
 struct MyRow {
     a: i64,
