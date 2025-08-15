f5cbce4b94f71032e5a2b932df451685a3728fea
Markus Klein <markus-klein@live.de>
2025-08-15 13:59:09 +0200

    docs: Improved documentation around Send for Connections

--- odbc-api/src/handles/statement_connection.rs
+++ odbc-api/src/handles/statement_connection.rs
@@ -54,23 +54,8 @@ pub unsafe trait ConnectionOwner {}
 ///
 /// Operations to a statement imply that interior state of the connection might be mutated,
 /// depending on the implementation detail of the ODBC driver. According to the ODBC documentation
-/// this could always be considered save, since Connection handles are basically described as
-/// `Sync`. Yet making connections `Send` could very well lead to different statements on different
-/// threads actually changing the connection at the same time and truly relying on the thread safety
-/// of the ODBC driver. I am sceptical. Especially if on Linux unixODBC would not be configured to
-/// protect connections.
-///
-/// Note to users of `unixodbc`: You may configure the threading level to make unixodbc
-/// synchronize access to the driver (and thereby making them thread safe if they are not thread
-/// safe by themself. This may however hurt your performance if the driver would actually be able to
-/// perform operations in parallel.
-///
-/// See: <https://stackoverflow.com/questions/4207458/using-unixodbc-in-a-multithreaded-concurrent-setting>
-///
-/// `StatementConnection` however also owns the connection exclusively. Since connections are `Send`
-/// it is reasonable to assume this would work even if implementers of the ODBC driver do not care
-/// in particular about thread safety.
-unsafe impl<C> Send for StatementConnection<C> where C: Send {}
+/// this could always be considered save.
+unsafe impl<C> Send for StatementConnection<C> where C: ConnectionOwner {}
 
 unsafe impl<C> AnyHandle for StatementConnection<C>
 where
