3b3fa7a1528e185cfef781c8f5ea9897ada9e0b9
Markus Klein <markus-klein@live.de>
2026-03-04 18:13:50 +0100

    refactor: extracte PreallocatedPolling into separate module

--- odbc-api/src/preallocated.rs
+++ odbc-api/src/preallocated.rs
@@ -1,6 +1,6 @@
 use crate::{
-    CursorImpl, CursorPolling, Error, ParameterCollectionRef, Sleep,
-    execute::{execute_with_parameters, execute_with_parameters_polling},
+    CursorImpl, Error, ParameterCollectionRef, PreallocatedPolling,
+    execute::execute_with_parameters,
     handles::{AsStatementRef, SqlText, Statement, StatementRef},
 };
 
@@ -525,83 +525,4 @@ where
     let cursor = unsafe { CursorImpl::new(statement) };
 
     Ok(cursor)
-}
-
-/// Asynchronous sibling of [`Preallocated`] using polling mode for execution. Can be obtained using
-/// [`Preallocated::into_polling`].
-pub struct PreallocatedPolling<S> {
-    /// A valid statement handle in polling mode
-    statement: S,
-}
-
-impl<S> PreallocatedPolling<S> {
-    fn new(statement: S) -> Self {
-        Self { statement }
-    }
-}
-
-impl<S> PreallocatedPolling<S>
-where
-    S: AsStatementRef,
-{
-    /// Executes a statement. This is the fastest way to sequentially execute different SQL
-    /// Statements asynchronously.
-    ///
-    /// # Parameters
-    ///
-    /// * `query`: The text representation of the SQL statement. E.g. "SELECT * FROM my_table;".
-    /// * `params`: `?` may be used as a placeholder in the statement text. You can use `()` to
-    ///   represent no parameters. Check the [`crate::parameter`] module level documentation for
-    ///   more information on how to pass parameters.
-    /// * `sleep`: Governs the polling intervals
-    ///
-    /// # Return
-    ///
-    /// Returns `Some` if a cursor is created. If `None` is returned no cursor has been created (
-    /// e.g. the query came back empty). Note that an empty query may also create a cursor with zero
-    /// rows. Since we want to reuse the statement handle a returned cursor will not take ownership
-    /// of it and instead burrow it.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use odbc_api::{Connection, Error};
-    /// use std::{io::{self, stdin, Read}, time::Duration};
-    ///
-    /// /// Execute many different queries sequentially.
-    /// async fn execute_all(conn: &Connection<'_>, queries: &[&str]) -> Result<(), Error>{
-    ///     let mut statement = conn.preallocate()?.into_polling()?;
-    ///     let sleep = || tokio::time::sleep(Duration::from_millis(20));
-    ///     for query in queries {
-    ///         println!("Executing {query}");
-    ///         match statement.execute(&query, (), sleep).await {
-    ///             Err(e) => println!("{}", e),
-    ///             Ok(None) => println!("No results set generated."),
-    ///             Ok(Some(cursor)) => {
-    ///                 // ...print cursor contents...
-    ///             },
-    ///         }
-    ///     }
-    ///     Ok(())
-    /// }
-    /// ```
-    pub async fn execute(
-        &mut self,
-        query: &str,
-        params: impl ParameterCollectionRef,
-        sleep: impl Sleep,
-    ) -> Result<Option<CursorPolling<StatementRef<'_>>>, Error> {
-        let query = SqlText::new(query);
-        let stmt = self.statement.as_stmt_ref();
-        execute_with_parameters_polling(stmt, Some(&query), params, sleep).await
-    }
-}
-
-impl<S> AsStatementRef for PreallocatedPolling<S>
-where
-    S: AsStatementRef,
-{
-    fn as_stmt_ref(&mut self) -> StatementRef<'_> {
-        self.statement.as_stmt_ref()
-    }
 }
