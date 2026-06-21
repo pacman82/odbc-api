fe1f7027e8c2cfc51be433dc6fd598be8508711d
Markus Klein <markus-klein@live.de>
2026-06-21 16:49:23 +0000

    test: Assert that we can read results row by row using mdbtools

--- odbc-api/tests/integration/mdbtools.rs
+++ odbc-api/tests/integration/mdbtools.rs
@@ -1,6 +1,6 @@
 #![cfg(target_os = "linux")]
 
-use odbc_api::{ConnectionOptions, environment};
+use odbc_api::{ConnectionOptions, Cursor, environment};
 
 // nwind.mdb is the Northwind sample database downloaded from:
 // https://github.com/mdbtools/mdbtestdata/blob/master/data/nwind.mdb
@@ -14,12 +14,17 @@ const MDBTOOLS_CONNECTION: &str = concat!(
 /// mdbtools does not support SQL_ATTR_PARAMSET_SIZE; executing a prepared statement with no
 /// parameters must not call SQLSetStmtAttr for that attribute.
 #[test]
-fn execute_prepared_without_parameters() {
+fn execute_query() {
     let env = environment().unwrap();
     let conn = env
         .connect_with_connection_string(MDBTOOLS_CONNECTION, ConnectionOptions::default())
         .unwrap();
-    let mut _cursor = conn
-        .execute("SELECT ProductName FROM Products", (), None)
+    let mut cursor = conn
+        .execute("SELECT ProductName FROM Products WHERE ProductID = 1", (), None)
+        .unwrap()
         .unwrap();
+    let mut row = cursor.next_row().unwrap().unwrap();
+    let mut field = Vec::new();
+    row.get_text(1, &mut field).unwrap();
+    assert_eq!(&b"Chai"[..], field.as_slice());
 }
